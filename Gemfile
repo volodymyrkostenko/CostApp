9aa631541348d36403b2b7d39ac0e67a6406b410
Vova <volodymyrpodilskyy@gmail.com>
2018-06-11 17:49:34 +0300

    authenticate update

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+gem 'devise', '~> 4.4', '>= 4.4.3'
 
 gem 'rubyzip', '= 1.0.0'
 gem 'axlsx', '= 2.0.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories
   resources :projects do
     resources :items
   end
-  
+  root to: 'projects#index'
+  devise_for :users, controllers: { registrations: "registrations"}
 end
